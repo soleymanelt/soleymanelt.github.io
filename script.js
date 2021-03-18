9dc14165d29b97c3c3a37245733e174e6a2dd316
soleymanelt <amin.latreche@yahoo.fr>
2021-03-18 07:17:23 +0100

    Finalisation de la partie "Mes créations" / Réorganisation du code CSS

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ setInterval(function() {
         counter = 1;
     }
 }, 5000);
+
+var slides = document.querySelector("#container-3 .slides");
+var previous = document.querySelector(".previous");
+var next = document.querySelector(".next");
+
+next.onclick = function() {
+    slides.style.marginLeft = "-1480px";
+    next.style.opacity = "0";
+    next.style.cursor = "default";
+    previous.style.opacity = "1";
+    previous.style.cursor = "pointer";
+}
+
+previous.onclick = function() {
+    slides.style.marginLeft = "0px";
+    previous.style.opacity = "0";
+    previous.style.cursor = "default";
+    next.style.opacity = "1";
+    next.style.cursor = "pointer";
+}
